39b9d6489da03733df1003c5f4810450fdbd165b
jflores1 <jflores1@alumni.nd.edu>
2012-09-27 15:02:38 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fifthrails::Application.config.secret_token = '226be8b7abf773390c94004074bf3ac89c4d58f3d83f6a5fce4d43dedae2f4d1bea5960989449d9d4bddcae907a438961856227afba9f772f2637388cc568fac'
