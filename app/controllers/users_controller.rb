fb48bc8452ef7e804636cbfa7456dba39929acc1
jflores1 <jflores1@alumni.nd.edu>
2012-09-30 23:55:57 -0400

    Began work on users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_filter :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -19,15 +21,36 @@ class UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Password saved"
+      sign_in @user
+      redirect_to @user
+    else
+      redirect_to 'edit'
+    end
 
   end
 
   def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
+  private
+
+  def signed_in_user
+    redirect_to signin_url, notice: "Please sign in." unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,16 @@ require 'spec_helper'
 
 describe UsersController do
 
+  let(:user) {FactoryGirl.create(:user)}
+
+  it "should show the current user" do
+    User.stub(:find) {:user}
+  end
+
+  it "should create new users" do
+    @user = User.new(email_address:"jesse@test2.com", password:"newpassword", password_confirmation:"newpassword")
+    expect {@user.save!}.to change(User, :count).by(1)
+    expect {response.should redirect_to(user_path)}
+  end
+
 end
