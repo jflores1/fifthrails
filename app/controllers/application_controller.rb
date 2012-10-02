fd6d408b18d41df237dbc211e35d85665dc713f7
jflores1 <jflores1@alumni.nd.edu>
2012-10-02 18:51:49 -0400

    Updated header algorithm to working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  before_filter :get_current_user=
+  before_filter :get_current_user
 
-  def get_current_user=(user)
-    @current_user = user
+  def get_current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email_address(params[:session][:email_address].downcase)
     if user && user.authenticate(params[:session][:password])
-      sign_in
+      sign_in user
       redirect_to user
     else
       flash.now[:error] = "Invalid email/password combination"
